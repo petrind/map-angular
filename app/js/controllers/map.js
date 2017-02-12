05cc5538d1b196e314c55ffcb607773d5446610b
Petrus F. Subekti <petrus@xtremax.com>
2017-02-12 22:39:18 +0700

    [Update] Function to call directions if available

--- app/js/controllers/map.js
+++ app/js/controllers/map.js
@@ -3,56 +3,75 @@ angular.module('Map.controllers')
   'Toast', 'Origins','Destinations' , 'Common',
   function($rootScope, $scope, $mdDialog, Toast, Origins, Destinations, Common) {
 
-    $scope.common = Common;
+    $scope.common = Common; //common function
+    //replace the icon for origin
     $scope.originIcon = {
         "scaledSize": [64, 64],
         "url": "http://icons.iconarchive.com/icons/icons-land/vista-map-markers/256/Map-Marker-Marker-Outside-Azure-icon.png"
     };
+    //replace the icon for destination
     $scope.destinationIcon = {
       "scaledSize":[64,64],
       "url": "http://icons.iconarchive.com/icons/icons-land/vista-map-markers/256/Map-Marker-Marker-Outside-Pink-icon.png"
     }
-	  $scope.vm = this;    
-    $rootScope.data.mode = "none";
-    $rootScope.data.tempPoint = {};
-    $scope.newDestination ={};
-    $scope.newOrigin ={};
 
+	  $scope.vm = this;
+    $rootScope.data.mode = "none"; //mode for editing data
+    $rootScope.directions = {}; //data for maps directions
+    $rootScope.data.tempPoint = {}; //temp point of clickpoint of map
+    $scope.newOrigin ={}; // temp variable to store newOrigin
+    $scope.newDestination ={}; // temp variable to store new Destination
+    $scope.distance = 0;
+
+    
+    //initiate waypoints of maps directions
     $scope.initWaypoints = function () {
-      $rootScope.wayPoints = [];
-      var maxDistance = 0;
-      var keyMax = 0;
-      angular.forEach($rootScope.selectedOrigin.Destinations, 
-        function(destination, key){
-            var distance = $scope.common.calculateSimpleDistance($rootScope.selectedOrigin, destination);
-            if(maxDistance<distance){
-              maxDistance = distance;
-              keyMax = key;
-            }
-            $rootScope.wayPoints.push(
-              {location: {lat:destination.latitude, lng: destination.longitude}, stopover: false}
-            );
-        });
-      $rootScope.last = $rootScope.wayPoints[keyMax];
-      $rootScope.wayPoints.splice(keyMax,1);
-      $rootScope.mode= "DRIVING";
-    }
+      if($rootScope.selectedOrigin.Destinations.length>0){
+        $rootScope.directions.show = true;
+        $rootScope.directions.wayPoints = [];
+        var maxDistance = 0;
+        var keyMax = 0;
+        //loop to push destination to wayPoints and find the farthest point
+        angular.forEach($rootScope.selectedOrigin.Destinations, 
+          function(destination, key){
+              var distance = $scope.common
+              .calculateSimpleDistance($rootScope.selectedOrigin, destination);
+              if(maxDistance<distance){
+                maxDistance = distance;
+                keyMax = key;
+              }
+              $rootScope.directions.wayPoints.push(
+                {location: {lat:destination.latitude, lng: destination.longitude}, stopover: false}
+              );
+          });
+        $rootScope.directions.first = {
+          location: {
+            lat: $rootScope.selectedOrigin.latitude, 
+            lng: $rootScope.selectedOrigin.longitude
+          }};
 
-    $scope.init = function () {
-
-    }
-    $scope.init();
-
-    $rootScope.$on('updateDirections', function(event, mass) { 
-       if($rootScope.selectedOrigin.Destinations != null && 
-        $rootScope.selectedOrigin.Destinations != undefined){
-        $scope.initWaypoints();
+        $rootScope.directions.last = $rootScope.directions.wayPoints[keyMax]; //last destination
+        $rootScope.directions.wayPoints.splice(keyMax,1); //remove last destination from waypoints
+        $rootScope.directions.mode= "DRIVING";
+      } else {
+        $rootScope.directions.show = false;
+        $rootScope.directions.wayPoints = [];
+        $rootScope.directions.last.location ={};
+        $rootScope.directions.first.location ={};
       }
+      $rootScope.data.mode = "none";
+      $rootScope.data.tempPoint = {};
+    }
+    //update Directions event emited by main.js
+    $rootScope.$on('updateDirections', function(event, mass) { 
+        $scope.initWaypoints();      
     });
-
+    //addMarker function 
     $scope.vm.addMarker = function(event) {
-      var ll = event.latLng;
-      $rootScope.data.tempPoint = {latitude: ll.lat(), longitude: ll.lng()};
+      var ll = event.latLng; //latitude and longitude from ngmap click event
+      if($rootScope.data.mode !== "none"){
+        $rootScope.data.tempPoint = {latitude: ll.lat(), longitude: ll.lng()};
+      }
       if($rootScope.data.mode === "new-origin"){
         $scope.newOrigin.latitude = $rootScope.data.tempPoint.latitude;
         $scope.newOrigin.longitude= $rootScope.data.tempPoint.longitude;
@@ -67,29 +86,33 @@ angular.module('Map.controllers')
         $rootScope.selectedDestination.longitude= $rootScope.data.tempPoint.longitude;
       }
     }
-
+    //save new origin point
     $scope.vm.saveOrigin = function (origin) {
-      Origins
-        .new(origin)
-        .then(function (neworigin) {
-          $scope.newOrigin ={};
-          $rootScope.data.mode = "none";
-          neworigin.Destinations = [];
-          $rootScope.origins.push(neworigin);
-          Toast.show('Origin successfully created', 'top right', 3000);
-          $mdDialog.cancel();
-        })
-        .catch(function () {
-          Toast.show('Error creating origin', 'top right', 3000);
-        });
-
+      if(!$scope.common.pointValidation(origin)){
+        Toast.error('Please click on map to specify location', 'top right', 3000);
+      } else{
+        if(origin.label===null || origin.label===undefined){
+          origin.label = "Origin Point "+ $rootScope.origins.length;
+        }
+        Origins
+          .new(origin)
+          .then(function (neworigin) {
+            $scope.newOrigin ={}; //reset
+            neworigin.Destinations = [];
+            $rootScope.origins.push(neworigin);
+            Toast.show('Origin successfully created', 'top right', 3000);
+            $mdDialog.cancel();
+          })
+          .catch(function () {
+            Toast.error('Error creating origin', 'top right', 3000);
+          });
+      }  
     };
-    //Update a destination's details
+    //Update a destination's details, Not used for now
     $scope.updateOrigin = function (origin) {
       Origins
         .update(origin)
         .then(function () {
-          $rootScope.data.mode = "none";
           $scope.initWaypoints();
           Toast.show('Destination successfully updated', 'top right', 3000);
           $mdDialog.cancel();
@@ -101,34 +124,42 @@ angular.module('Map.controllers')
 
     //create a new destination
     $scope.vm.saveDestination = function (destination) {
-      destination['origin_id'] = $rootScope.selectedOrigin.id;
-      Destinations
+      if(!$scope.common.pointValidation(destination)){
+        Toast.error('Please click on map to specify location', 'top right', 3000);
+      } else {
+        if(destination.label===null || destination.label===undefined){
+          destination.label = "Destination Point "+ $rootScope.selectedOrigin.Destinations.length;
+        }
+        destination.origin_id = $rootScope.selectedOrigin.id;
+        Destinations
         .new(destination)
         .then(function (newDestination) {
-          $rootScope.data.mode = "none";
-          $scope.initWaypoints();
-          $scope.newDestination ={};
           Toast.show('Destination successfully created', 'top right', 3000);
           $rootScope.selectedOrigin.Destinations.push(newDestination);
+          $scope.initWaypoints();
           $mdDialog.cancel();
         })
         .catch(function () {
-          Toast.show('Error creating destination', 'top right', 3000);
+          Toast.error('Error creating destination', 'top right', 3000);
         });
+      }      
     };
 
     //Update a destination's details
     $scope.vm.updateDestination = function (destination) {
-      Destinations
+      if(!$scope.common.pointValidation(destination)){
+        Toast.error('Please click on map to specify location', 'top right', 3000);
+      } else {
+        Destinations
         .update(destination)
-        .then(function () {
-          $rootScope.data.mode = "none";
-          $scope.initWaypoints();
+        .then(function () {          
           Toast.show('Destination successfully updated', 'top right', 3000);
+          $scope.initWaypoints();
           $mdDialog.cancel();
         })
         .catch(function () {
-          Toast.show('Error updating destination', 'top right', 300);
+          Toast.error('Error updating destination', 'top right', 300);
         });
+      }      
     };
 	}]);
