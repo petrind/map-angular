dbb0337f04ee861183578de9330a0fde4b66395b
Petrus F. Subekti <petrus@xtremax.com>
2017-02-12 22:35:37 +0700

    [Update] Config provide

---
+++ app/js/services/config.js
@@ -0,0 +1,12 @@
+angular.module('Map.services')
+  .factory('Config', ['$http', '$q', function ($http, $q) {
+  	return{
+      defaultLocation : function() {
+        return {
+          latitude : -6.919269633161684,
+          longitude : 107.61739253997803,
+          Destinations : []
+        };
+      }
+    };
+  }]);
