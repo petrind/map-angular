39ba28f1883bc1cfe8e86b4c45290a661b353819
Petrus F. Subekti <petrus@xtremax.com>
2017-02-12 22:38:28 +0700

    [Update] Validate location, refactor common provider

--- app/js/services/common.js
+++ app/js/services/common.js
@@ -1,12 +1,19 @@
 angular.module('Map.services')
   .factory('Common', ['$http', '$q', function ($http, $q) {
-  	var common = {};
-  	//calculate simple distance using pythagoras
-    common.calculateSimpleDistance = function(origin, destination){
-    	var a = origin.latitude - destination.latitude;
-    	var b = origin.longitude - destination.longitude;
+  	return{
+      calculateSimpleDistance : function(origin, destination){
+      	var a = origin.latitude - destination.latitude;
+      	var b = origin.longitude - destination.longitude;
         var distance = Math.sqrt( a*a + b*b );
         return distance;
+      },
+      pointValidation : function(point){
+        if((point.latitude===null || point.latitude===undefined) || 
+        (point.longitude===null || point.longitude===undefined)){
+          return false;
+        } else {
+          return true;
+        }
+      }
     };
-    return common;
   }]);
